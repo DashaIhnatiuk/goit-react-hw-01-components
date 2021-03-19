494cdf1554fcc2e206eb781c3d75b5f7ef5fe137
Anton Vodorez <vodorez.anton07@gmail.com>
2021-03-20 01:10:59 +0200

    Перенос рендера компонентов в App.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Profile from './task1/Profile';
-import Stats from './task2/Stats';
-import FriendList from './task3/FriendList';
-import reportWebVitals from './reportWebVitals';
-import TableList from './task4/Table';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Profile />
-    <Stats/>
-    <FriendList />
-<TableList/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
